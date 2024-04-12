b7da724d4686bc576437b52690ab93683246bde8
Damir <d.zaripov@uchi.ru>
2024-04-13 01:32:11 +0300

    cleanup hook implemented

--- conftest.py
+++ conftest.py
@@ -6,3 +6,9 @@ pytest_plugins = [
     'fixtures.page',
     'fixtures.unbind_user'
 ]
+
+
+# запуск клинеров, если тесты упали - можно указать несколько фикстур
+def pytest_runtest_makereport(item, call):
+    if call.excinfo is not None:
+        item.fixturenames.append("unbind_user")
