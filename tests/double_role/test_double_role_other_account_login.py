1dea949432809f6a81f0c07d00d327affa3b22d9
Damir <d.zaripov@uchi.ru>
2024-07-10 12:35:06 +0300

    a little refactor

--- tests/double_role/test_double_role_other_account_login.py
+++ tests/double_role/test_double_role_other_account_login.py
@@ -8,8 +8,9 @@ from pages.oauth_login_page import MockLogin
 
 
 @pytest.mark.regression
+@pytest.mark.parametrize("unbind_user", ["mock-teacher-parent"], indirect=True)
 class TestDoubleRoleOtherAccountLogin:
-    def test_double_role_other_account_login(self, browser):
+    def test_double_role_other_account_login(self, browser, unbind_user):
         login = MockLogin(browser)
         login.mock_double_role_other_account_login()
         login.is_on_double_role_uchi_login()
